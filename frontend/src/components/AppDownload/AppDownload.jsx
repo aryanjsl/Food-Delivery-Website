9833a3f8e1095bc1cdeefc03e62b9e438437c080
Aryan Jaiswal <aryanjsl999@gmail.com>
2025-04-05 18:25:13 +0530

    Changes in Navbar

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -4,7 +4,7 @@ import { assets } from '../../assets/assets'
 const AppDownload = () => {
   return (
     <div className='app-download' id='app-download'>
-        <p>For seamless performance, download now! <br /> Bella Onojie App</p>
+        <p>A better way to eat<br />Subscription-based Homemade meal plans launching soon!</p>
         <div className="app-download-platforms">
             <img src={assets.play_store} alt="" />
             <img src= {assets.app_store} alt="" />
