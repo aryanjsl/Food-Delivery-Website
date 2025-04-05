6b11694e0acf9cfb28fa3d2c6c43b13c78710791
Aryan Jaiswal <aryanjsl999@gmail.com>
2025-04-05 16:22:26 +0530

    New Food items added

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -33,7 +33,7 @@ const Footer = () => {
         </div>
       </div>
       <hr />
-      <p className="footer-copyright">By continuing past this page, you agree to our Terms of Service, Cookie Policy, Privacy Policy and Content Policies. All trademarks are properties of their respective owners. 2025 © Bella Onojie™ Ltd. All rights reserved.</p>
+      <p className="footer-copyright">By continuing past this page, you agree to our Terms of Service, Cookie Policy, Privacy Policy and Content Policies. All trademarks are properties of their respective owners. 2025 © Big Bites™ Ltd. All rights reserved.</p>
     </div>
   )
 }
