fd8d9a8546e6d5bd7bf347ec8857cbbc799e4760
Aryan Jaiswal <aryanjsl999@gmail.com>
2025-04-08 22:48:37 +0530

    New

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Header.css'
-import headerImg from '../assets/header_img.png';
+import headerImg from '../../assets/header_img.png';
+
 const Header = () => {
   return (
     <div className='header'
