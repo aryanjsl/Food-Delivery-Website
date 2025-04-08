eeb4f240c2df96d60cadcc1c28c9265650617adc
Aryan Jaiswal <aryanjsl999@gmail.com>
2025-04-08 22:41:17 +0530

    New Change

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import './Header.css'
+import headerImg from '../assets/header_img.png';
 const Header = () => {
   return (
-    <div className='header'>
+    <div className='header'
+    style={{
+      backgroundImage: `url(${headerImg})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'contain',
+    }}>
       <div className='header-contents'>
         <h2>Order fresh, tasty meals now!</h2>
         <p>Order fresh, tasty meals now! Enjoy homemade, restaurant-quality food delivered to your doorstep. Our meal plans keep you healthy and satisfied with fresh, high-quality ingredients. Say goodbye to meal prep stress and enjoy hassle-free dining!</p>
